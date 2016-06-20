57b7ef09358674d844353e2769ec28d2db4a488b
TingYao Hsu <tingyao.hsu@gmail.com>
2016-06-20 17:01:51 +0800

    Add coturn service

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,6 +9,22 @@ if [ ! -z "$COTURN_PORT_3478_TCP_ADDR" ]; then
   fi
 fi
 
+EXTERNAL_IP=$(curl -4 icanhazip.com)
+
+echo "TURNSERVER_ENABLED=1" >> /etc/default/coturn \
+	&& echo "user=kurento:kurento" >> /etc/turnserver.conf \
+	&& echo "realm=kurento.org" >> /etc/turnserver.conf
+	
+if [ ! -z "$IS_AWS_EC2" ]; then
+	# Launch Turnserver if have external IP
+	EXTERNAL_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+	LOCAL_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+	echo "external-ip=${EXTERNAL_IP}/${LOCAL_IP}" >> /etc/turnserver.conf
+else
+	echo "external-ip=${EXTERNAL_IP}/${LOCAL_IP}" >> /etc/turnserver.conf
+fi
+
+service coturn start
 # Remove ipv6 local loop until ipv6 is supported
 cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
 
